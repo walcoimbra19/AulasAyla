819cf7a50eb7a5c65c91f2102514c5841eaf14d2
junior <junior@junior-pc>
2017-08-28 10:57:08 -0300

    a

--- AULASPOO/src/SistemaViacaoRT/Passagem.java
+++ AULASPOO/src/SistemaViacaoRT/Passagem.java
@@ -34,5 +34,32 @@ public class Passagem {
 	public void setIdUsuario(String idUsuario) {
 		this.idUsuario = idUsuario;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idUsuario == null) ? 0 : idUsuario.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Passagem other = (Passagem) obj;
+		if (idUsuario == null) {
+			if (other.idUsuario != null)
+				return false;
+		} else if (!idUsuario.equals(other.idUsuario))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
 	
 }
--- AULASPOO/src/SistemaViacaoRT/Usuario.java
+++ AULASPOO/src/SistemaViacaoRT/Usuario.java
@@ -1,6 +1,7 @@
 package SistemaViacaoRT;
 
 public class Usuario {
+	
 	private String idUsuario;
 	private String nomeUsuario;
 	private String tipoUsuario;
@@ -32,6 +33,29 @@ public class Usuario {
 	}
 	public void setTipoUsuario(String tipoUsuario) {
 		this.tipoUsuario = tipoUsuario;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idUsuario == null) ? 0 : idUsuario.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (idUsuario == null) {
+			if (other.idUsuario != null)
+				return false;
+		} else if (!idUsuario.equals(other.idUsuario))
+			return false;
+		return true;
 	}
 	
 }
